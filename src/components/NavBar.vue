38653a1a15d95a92a9f66fb63280f5345183dfae
Mark Lawrence <lawrence.ml@gmail.com>
2020-08-10 21:47:02 -0400

    display list of categories

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,3 +1,29 @@
 <template>
-  <div>NavBar</div>
+  <ul class="list-reset inline flex justify-center border-b-4 mb-0">
+    <li
+      class="p-4 inline bg-gray-200 hover:bg-gray-400 uppercase font-black cursor-pointer"
+      v-for="category in categories"
+      :key="category"
+    >
+      {{ category }}
+    </li>
+
+    <li
+      class="p-4 inline bg-gray-200 hover:bg-gray-400 uppercase font-black cursor-pointer"
+      @click="triggerShowAddCategory"
+    >
+      ➕
+    </li>
+  </ul>
 </template>
+
+<script>
+export default {
+  props: ["categories"],
+  methods: {
+    triggerShowAddCategory() {
+      this.$emit("triggerShowAddCategory");
+    },
+  },
+};
+</script>
